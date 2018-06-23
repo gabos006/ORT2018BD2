435c0a757efb029bb77f917f020ca152865ca142
Gabriel Pereyra <Gabriel@PC-Gabo.(none)>
2018-06-23 11:49:08 -0300

    Requerimiento produccion papel

--- Procedures.sql
+++ Procedures.sql
@@ -54,8 +54,7 @@ END;
 -- REQUERIMIENTO GENERACION DE ENERGIA
 CREATE OR REPLACE PROCEDURE GENERACION_ENERGIA(FECHA_DESDE IN DATE, FECHA_HASTA IN DATE) AS
 BEGIN
-    DECLARE
-        
+    DECLARE       
         cant_madera_lote NUMBER(10);
         cant_madera_chip NUMBER(10);
         
@@ -65,26 +64,12 @@ BEGIN
             FROM ENERGIA E
             WHERE E.FECHA >= FECHA_DESDE AND E.FECHA <= FECHA_HASTA
             GROUP BY E.FECHA
-            ORDER BY E.FECHA;
-
-       /* -- AGRUPO POR FECHA
-        CURSOR GRUPO IS
-            SELECT E.FECHA AS FECHA,SUM(KW) AS KW,SUM(M.PESOMADERALOTE) AS PESOMADERA,SUM(M.PESOCHIP) AS PESOCHIP 
-            FROM ENERGIA E, COCCION C, MADERACHIP M
-            WHERE M.ID = C.IDCHIP AND C.ID = E.IDENERGIA
-                  AND E.FECHA >= FECHA_DESDE AND E.FECHA <= FECHA_HASTA
-                  AND C.FECHA >= FECHA_DESDE AND C.FECHA <= FECHA_HASTA
-                  AND M.FECHA >= FECHA_DESDE AND M.FECHA <= FECHA_HASTA
-            GROUP BY E.FECHA
-            ORDER BY E.FECHA;
-*/
+            ORDER BY E.FECHA;         
     BEGIN
-    
         DBMS_OUTPUT.PUT_LINE('FECHA' || '     ' || 'CANT. KW' || '     ' || 'PESO LOTE' || '     ' || 'PESO CHIP');
         
         FOR itemEnergia IN ENERGIA
         LOOP
-            
             SELECT SUM(M.PESOMADERALOTE) INTO cant_madera_lote
             FROM MADERACHIP M
             WHERE M.FECHA = itemEnergia.FECHA;
@@ -94,12 +79,39 @@ BEGIN
             WHERE C.FECHA = itemEnergia.FECHA;
             
             DBMS_OUTPUT.PUT_LINE(rpad(itemEnergia.FECHA,12) || rpad(itemEnergia.KW,12) || rpad(cant_madera_lote,16) || rpad(cant_madera_chip,12));        
-        END LOOP;
+        END LOOP;  
+    END;
+END;
+
+-- Funciona como DELIMITER
+/
+
+CREATE OR REPLACE PROCEDURE PRODUCCION_PAPEL(FECHA_DESDE IN DATE, FECHA_HASTA IN DATE) AS
+BEGIN
+    DECLARE
+        v_fecha DATE;
+        v_sum_mchip NUMBER(10);
+        v_sum_lmadera NUMBER(10);
+        -- AGRUPO POR FECHA
+        CURSOR GRUPO IS
+            SELECT SUM(p.PESO) AS SUMA, p.FECHA as FECHA FROM PAPEL p WHERE p.FECHA >= FECHA_DESDE AND p.FECHA <= FECHA_HASTA
+            GROUP BY p.FECHA
+            ORDER BY p.FECHA;
+    BEGIN
+        DBMS_OUTPUT.PUT_LINE('FECHA' || '     ' || 'PESO PAPEL' || '     ' || 'PESO CHIPS' || '     ' || 'PESO LOTE');
         
+
+        FOR itemGrupo IN GRUPO
+        LOOP
+                SELECT NVL(SUM(m.PESOCHIP),0) into v_sum_mchip FROM MADERACHIP m WHERE m.FECHA = itemGrupo.FECHA;
+                SELECT NVL(SUM(l.PESOINICIAL),0) into v_sum_lmadera FROM LOTEMADERA l WHERE l.FECHA = itemGrupo.FECHA;
+                DBMS_OUTPUT.PUT_LINE(rpad(itemGrupo.FECHA,12) || rpad(itemGrupo.SUMA,14) || rpad(v_sum_mchip,14) || rpad(v_sum_lmadera,12)); 
+        END LOOP;
     END;
 END;
 
 -- TESTS
 --EXECUTE MADERA_ALMACENADA();
 --EXECUTE MADERA_MAL_ESTADO(CURRENT_DATE);
-EXECUTE GENERACION_ENERGIA('18/06/2018','23/06/2018');
+--EXECUTE GENERACION_ENERGIA('18/06/2018','23/06/2018');
+--EXECUTE PRODUCCION_PAPEL('18/06/2018','23/06/2018');
